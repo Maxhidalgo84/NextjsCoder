d4b237938a5feb571f98a61ba7776549855d3ffc
Maximo <maxhidalgo84@gmail.com>
2023-12-17 20:47:36 -0300

    finalbeta 2

--- components/context/CartContext.js
+++ components/context/CartContext.js
@@ -9,16 +9,22 @@ export const useCartContext = () => useContext(CartContext)
 
 export const CartProvider = ({ children }) => {
 
+    const [isMounted, setIsMounted] = useState(false);
 
+
+    let cartLocal = []
     
-    
-    let cartLocal =  JSON.parse(localStorage.getItem("cart"));
-    if (cartLocal === null) {
-        cartLocal = [];
+    if (global?.window !== undefined) {
+         cartLocal =  JSON.parse(localStorage.getItem("cart"));
+        if (cartLocal === null) {
+            cartLocal = [];
+        }
     }
 
     const [cart, SetCart] = useState(cartLocal)
 
+
+   
 
 
     const isInCart = (slug) => cart.find(item => item.slug === slug);
@@ -43,6 +49,14 @@ export const CartProvider = ({ children }) => {
     useEffect(() => {
         localStorage.setItem("cart", JSON.stringify(cart));
     }, [cart]);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
 
 
 
