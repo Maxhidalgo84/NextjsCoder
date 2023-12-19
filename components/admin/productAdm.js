0f61f2648be3a6db90eff7307685b24e5edda0d2
Maximo <maxhidalgo84@gmail.com>
2023-12-19 18:04:54 -0300

    admin edit

--- components/admin/productAdm.js
+++ components/admin/productAdm.js
@@ -37,8 +37,7 @@ const ProductAdm = async ({ slug }) => {
                 <div className="px-4 sm:px-0 flex justify-between">
                     <h3 className="text-base font-semibold leading-7 mx-auto text-gray-900">Edicion del producto</h3>
                 </div>
-                <p>{product.slug}</p>
-                {/* <ProductEdit product={product} /> */}
+                {product && <ProductEdit product={product} />}
 
 
             </div>
